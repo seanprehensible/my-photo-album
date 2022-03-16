b8f8a315026f2bdf428a8f82f919cd460b1aeefa
seanprehensible <seanprehensible@gmail.com>
2022-03-16 14:53:14 +0900

    Add submit logic

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const UplaodForm = () => {
+  const defaultMsg = "이미지 파일을 업로드 해주세요.";
+  const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState(defaultMsg);
+
+  const imageSelectHandler = (event) => {
+    console.log(event);
+    const imageFile = event.target.files[0];
+    setFile(imageFile);
+    setFileName(imageFile.name);
+  };
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("image", file);
+    try {
+      const res = await axios.post("/upload", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      console.log(res);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <form>
-      <label htmlFor="image">사진</label>
-      <input id="image" type="file" />
+    <form onSubmit={onSubmitForm}>
+      <label htmlFor="image">{fileName}</label>
+      <input id="image" type="file" onChange={imageSelectHandler} />
       <button type="submit">업로드</button>
     </form>
   );
