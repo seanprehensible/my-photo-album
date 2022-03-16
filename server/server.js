42a5239cbddd914b8dab86b45fa46905ca2a1c35
seanprehensible <seanprehensible@gmail.com>
2022-03-16 14:53:43 +0900

    Update port number

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const { v4: uuid } = require("uuid");
 const mime = require("mime-types");
 
 const app = express();
-const PORT = 3000;
+const PORT = 5050;
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
