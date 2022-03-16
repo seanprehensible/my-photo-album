39e4b920f2e73c857ecae6cd9d383d3602a517ac
seanprehensible <seanprehensible@gmail.com>
2022-03-16 15:23:22 +0900

    Add upload status alert

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import UplaodForm from "./components/UploadForm";
+import { ToastContainer } from "material-react-toastify";
+import "material-react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <div>
       <h2>사진첩</h2>
       <UplaodForm />
+      <ToastContainer />
     </div>
   );
 };
--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./UploadForm.css";
+import { toast } from "material-react-toastify";
 
 const UplaodForm = () => {
   const defaultMsg = "이미지 파일을 업로드 해주세요.";
@@ -23,8 +24,10 @@ const UplaodForm = () => {
         headers: { "Content-Type": "multipart/form-data" },
       });
       console.log(res);
+      toast.success("업로드 성공!");
     } catch (err) {
       console.error(err);
+      toast.error("업로드 실패!");
     }
   };
 
